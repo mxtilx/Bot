bd5c301072843b6ec8f510b9223e7484e7283035
Yuuki <me@yuuki.me>
2023-05-10 23:04:06 +0800

    add dump sr (for testing login)

--- src/commands/res.js
+++ src/commands/res.js
@@ -5,6 +5,7 @@ const { SlashCommandBuilder, CommandInteraction } = require("discord.js")
 const log = require("../util/logger")
 
 const api_genshin = require("../game/genshin/api")
+const api_starrails = require("../game/starrails/api")
 
 const lib = require("../lib")
 
@@ -13,7 +14,8 @@ module.exports = {
 		.setName("res")
 		.setDescription("Get debug res")
 		.addStringOption((option) => option.setName("seed").setDescription("Seed ID").setRequired(true))
-		.addStringOption((option) => option.setName("version").setDescription("Game Version").setRequired(true)),
+		.addStringOption((option) => option.setName("version").setDescription("Game Version").setRequired(true))
+		.addStringOption((option) => option.setName("game").setDescription("Game ID, 1=GS 2=SR").setRequired(true)),
 	/**
 	 * @param {CommandInteraction} interaction
 	 * @returns {void}
@@ -22,25 +24,39 @@ module.exports = {
 		try {
 			let set_seed_id = interaction.options.getString("seed")
 			let set_version = interaction.options.getString("version")
+			let set_game = interaction.options.getString("game")
+
 			let id_user = interaction.user.id
 
 			interaction.reply({ content: "Please wait...", ephemeral: true })
 
 			await lib.sleep(2)
 
-			let d = await api_genshin.RES(set_version, set_seed_id)
+			let d = ""
+			if (set_game == "1") {
+				d = await api_genshin.RES(set_version, set_seed_id)
+			} else if (set_game == "2") {
+				d = await api_starrails.RES(set_version, set_seed_id)
+			} else {
+				return interaction.editReply({
+					content: `No found game`,
+					ephemeral: true
+				})
+			}
 
-			console.log(JSON.stringify(d))
+			var raw = JSON.stringify(d)
+			console.log(raw)
 
 			if (d.code != 200) {
 				await interaction.editReply({
 					content: d.msg,
 					ephemeral: true
 				})
-				return;
+				return
 			}
 
-			var da = `			
+			if (set_game == "1") {
+				var da = `			
 			cfResource.add(
 				new Resource(
 					"${set_version}",
@@ -65,11 +81,21 @@ module.exports = {
 				)
 			);			
 			`
-
-			return await interaction.editReply({
-				content: `\`\`\`\n${da}\n\`\`\``,
-				ephemeral: true
-			})
+				return await interaction.editReply({
+					content: `\`\`\`\n${da}\n\`\`\``,
+					ephemeral: true
+				})
+			} else if (set_game == "2") {
+				return await interaction.editReply({
+					content: `Found asset file: ${d.data._AssetBundleVersionUpdateUrl} ,for complete file check dump folder.`,
+					ephemeral: true
+				})
+			} else {
+				return await interaction.editReply({
+					content: `idk`,
+					ephemeral: true
+				})
+			}
 		} catch (err) {
 			log.error(err)
 			return await interaction.editReply({ content: "Unknown error", ephemeral: true })
